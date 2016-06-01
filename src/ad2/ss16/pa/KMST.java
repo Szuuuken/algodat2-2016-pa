71a5bdd1865a2ffa6aaa54137230848bc06902e7
Daniel Szukitsch <daniel@szuuuken.at>
2016-06-01 16:45:41 +0200

    prim; neue Datenstrucktur

--- src/ad2/ss16/pa/KMST.java
+++ src/ad2/ss16/pa/KMST.java
@@ -1,13 +1,18 @@
 package ad2.ss16.pa;
 
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Klasse zum Berechnen eines k-MST mittels Branch-and-Bound. Hier sollen Sie
  * Ihre L&ouml;sung implementieren.
  */
 public class KMST extends AbstractKMST {
-
+	private int min = Integer.MIN_VALUE;
+	private int max = Integer.MAX_VALUE;
+	private int k;
+	private ArrayList<HashSet<Edge>> problems = new ArrayList<HashSet<Edge>>();
+	private Set<Integer> nodes;
+	private HashMap<Integer,ArrayList<Edge>> paths;
 	/**
 	 * Der Konstruktor. Hier ist die richtige Stelle f&uuml;r die
 	 * Initialisierung Ihrer Datenstrukturen.
@@ -22,8 +27,41 @@ public class KMST extends AbstractKMST {
 	 *            Die Anzahl der Knoten, die Ihr MST haben soll
 	 */
 	public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
-		// TODO: Hier ist der richtige Platz fuer Initialisierungen
-	}
+		this.k = k;
+		problems.add(edges);
+		setSolution(numEdges, edges);
+
+		Graph graph = new Graph();
+
+		for (Edge edge:edges) {
+			graph.addEdge(edge);
+		}
+	}/*
+		nodes = new HashSet<Integer>();
+		paths = new HashMap<Integer, ArrayList<Edge>>();
+
+
+		for (Edge edge:edges) {
+			nodes.add(edge.node1);
+			nodes.add(edge.node2);
+
+			if(!paths.containsKey(edge.node1)){
+				paths.put(edge.node1,new ArrayList<Edge>());
+
+			}
+
+			paths.get(edge.node1).add(edge);
+
+			if(!paths.containsKey(edge.node2)){
+				paths.put(edge.node2,new ArrayList<Edge>());
+
+			}
+
+			paths.get(edge.node2).add(edge);
+
+		}
+
+	}*/
 
 	/**
 	 * Diese Methode bekommt vom Framework maximal 30 Sekunden Zeit zur
@@ -36,7 +74,101 @@ public class KMST extends AbstractKMST {
 	 */
 	@Override
 	public void run() {
-		// TODO: Diese Methode ist von Ihnen zu implementieren
+		Main.printDebug("run");
+
+		while(!problems.isEmpty()){
+			HashSet<Edge> subProblem = problems.get(0);
+			problems.remove(0);
+
+			int localMin = 2;
+
+			if(localMin < max){
+				HashSet<Edge> mst = prim(subProblem);
+				int localMax = mst;
+
+				if(localMax < max)
+					max = localMax;
+
+				if(localMin < max){
+					for()
+				}
+			}
+		}
 	}
 
+	private HashSet<Edge> prim(HashSet<Edge> graph){
+		HashSet<Edge> mst = new HashSet<Edge>();
+		HashSet<Integer> visited = new HashSet<Integer>();
+
+		for (int node:nodes) {
+			ArrayList<Edge> edges = paths.get(node);
+
+			Edge minEdge = null;
+			for (Edge edge:edges) {
+				int currentNode = -1;
+				int nextNode = -1;
+				if(edge.node1 == node){
+					currentNode = edge.node1;
+					nextNode = edge.node2;
+				}
+
+				if(minEdge == null) {
+					if(!visited.contains(nextNode))
+						minEdge = edge;
+				}
+				else{
+					if(edge.weight < minEdge.weight && !visited.contains(nextNode))
+						minEdge = edge;
+				}
+			}
+
+			if(minEdge != null)
+				mst.add(minEdge);
+
+			visited.add(node);
+		}
+
+
+		return mst;
+	}
+
+	public class Node{
+		private int value;
+		private ArrayList<Edge> edges;
+
+		public Node(int value){
+			this.value = value;
+			edges = new ArrayList<Edge>();
+		}
+
+		public int getValue(){return this.value;}
+		public ArrayList<Edge> getEdges(){return this.edges;}
+		public void addEdge(Edge edge){this.edges.add(edge);}
+	}
+
+	public class Graph{
+		private HashMap<Integer,Node> nodes;
+		private HashSet<Edge> edges;
+
+		public Graph(){
+			this.nodes = new HashMap<Integer, Node>();
+			this.edges = new HashSet<Edge>();
+		}
+
+		public void addEdge(Edge edge){
+			addOrGetNode(edge.node1).addEdge(edge);
+			addOrGetNode(edge.node2).addEdge(edge);
+			this.edges.add(edge);
+		}
+
+		private Node addOrGetNode(int value){
+			if(nodes.containsKey(value))
+				return nodes.get(value);
+
+			Node node = new Node(value);
+			nodes.put(value,node);
+
+			return node;
+		}
+	}
 }
